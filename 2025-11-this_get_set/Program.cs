c3308758b264d68f6271afd0457a0e7abca4c1ab
Marclo148 <marcel.j3103@gmail.com>
2025-11-19 10:53:23 +0100

    get / set

---
+++ 2025-11-this_get_set/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Person
+{
+    // Feld für den Namen
+    private string name;
+
+    // Konstruktor: Wird beim Erstellen eines Objekts aufgerufen
+    public Person(string name)
+    {
+        // 'this' verweist auf das aktuelle Objekt
+        this.name = name;
+    }
+
+    // Eigenschaft mit get und set
+    public string Name
+    {
+        // 'get' gibt den Wert des Feldes zurück
+        get { return name; }
+        // 'set' setzt den Wert des Feldes
+        set { name = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Erstellen eines neuen Objekts mit dem Konstruktor
+        Person person = new Person("Max");
+
+        // Zugriff auf die Eigenschaft (get)
+        Console.WriteLine(person.Name);
+
+        // Setzen eines neuen Namens (set)
+        person.Name = "Anna";
+        Console.WriteLine(person.Name);
+    }
+}
